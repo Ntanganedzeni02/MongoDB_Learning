7fc0fc0eed0735dbedccdebc81b393a17a9721e3
Ntanga <phidzaglimantanganedzeni@gmail.com>
2025-06-23 00:41:33 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const {mongodbClient} = require("mongodb");
+const uri = require("./atlas_uri");
+
+console.log(uri);
---
+++ atlas_uri.js
@@ -0,0 +1,2 @@
+module.exports = uri =
+    "mongodb+srv://phidzaglimantanganedzeni:ThoYnxfs6BlXHaOR@cluster0.gx93tvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
