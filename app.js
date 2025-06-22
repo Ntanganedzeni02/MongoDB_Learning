63036e61568f8c91330c57c49d2c118de8eec8a2
Ntanga <phidzaglimantanganedzeni@gmail.com>
2025-06-23 00:54:24 +0200

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
-const {mongodbClient} = require("mongodb");
+const {MongoClient} = require("mongodb");
 const uri = require("./atlas_uri");
 
 console.log(uri);
+const client = new MongoClient(uri);
+
+const dbName = "learning";
+
+const connectToDatabase = async () => {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB Atlas");
+        const db = client.db(dbName);
+        console.log(`Database selected: ${db.databaseName}`);
+    } catch (error) {
+        console.error("Error connecting to MongoDB Atlas:", error);
+    } finally {
+        await client.close();
+    }
+};
+
+const main = async () => {
+    try {
+        await connectToDatabase();
+    } catch (error) {
+        console.error("An error occurred in the main function:", error);
+    } finally {
+        await client.close();
+    }
+};
+
+main();
